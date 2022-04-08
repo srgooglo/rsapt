f79b9b7b908dba92f2a62bb475bc736a22cf813a
srgooglo <chippotato39@gmail.com>
2022-04-08 23:01:21 +0200

    fix configs

--- packages/gui/configs/.config.js
+++ packages/gui/configs/.config.js
@@ -1,7 +1,5 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
 import pkg from "../package.json"
-import Pages from "vite-plugin-pages"
 
 export default (config = {}) => {
     if (!config.resolve) {
@@ -11,10 +9,14 @@ export default (config = {}) => {
         config.server = {}
     }
 
-    config.server.port = process.env.listenPort ?? 8000
-    config.server.host = "0.0.0.0"
-    config.server.fs = {
-        allow: [".."]
+    config.mode = process.env.NODE_ENV
+
+    config.server = {
+        host: pkg.env.HOST,
+        port: pkg.env.PORT,
+        fs: {
+            allow: [".."]
+        }
     }
 
     config.envDir = path.join(__dirname, "environments")
@@ -25,30 +27,6 @@ export default (config = {}) => {
                 javascriptEnabled: true,
             }
         }
-    }
-
-    config.mode = process.env.NODE_ENV
-
-    config.root = path.join(__dirname, "../src/renderer")
-
-    config.plugins = [
-        react(),
-        Pages({
-            react: true,
-            extensions: ["jsx", "tsx"],
-        }),
-    ]
-
-    config.base = "./"
-
-    config.build = {
-        emptyOutDir: true,
-        outDir: "../../dist/renderer",
-    }
-
-    config.server = {
-        host: pkg.env.HOST,
-        port: pkg.env.PORT,
     }
 
     return config
--- packages/gui/configs/vite.renderer.ts
+++ packages/gui/configs/vite.renderer.ts
@@ -1,3 +1,6 @@
+import react from "@vitejs/plugin-react"
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+
 import path from "path"
 import { defineConfig } from "vite"
 import getConfig from "./.config.js"
@@ -5,6 +8,20 @@ import getConfig from "./.config.js"
 const config = getConfig()
 
 const fixed__dirname = path.resolve(__dirname, "../src/renderer/")
+
+config.root = path.join(__dirname, "../src/renderer")
+
+config.plugins = [
+  react(),
+  viteCommonjs()
+]
+
+config.base = "./"
+
+config.build = {
+  emptyOutDir: true,
+  outDir: "../../dist/renderer",
+}
 
 config.resolve.alias = {
   "~/": `${path.resolve(fixed__dirname, "src")}/`,
@@ -21,4 +38,5 @@ config.resolve.alias = {
 }
 
 // https://vitejs.dev/config/
+console.log(config)
 export default defineConfig(config)
