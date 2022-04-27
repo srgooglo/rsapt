5830e61b2a9b4a5f257dc7487329b66cfa2a8fc8
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-27 21:39:45 +0200

    added installation methods

--- packages/gui/src/renderer/src/extensions/installations.extension.js
+++ packages/gui/src/renderer/src/extensions/installations.extension.js
@@ -1,13 +1,45 @@
 import { Extension } from "evite"
 import { message } from "antd"
 
-export default class SettingsExtension extends Extension {
-    makeInstallation = (manifest) => {
+export default class InstallationsExtension extends Extension {
+    makeInstallation = (packageManifest, version) => {
         return new Promise(async (resolve, reject) => {
-            const result = await window.bridge.ipcRenderer.invoke("make_install", manifest)
-            console.log(result)
+            const packageId = (typeof packageManifest === "object") ? packageManifest.name : packageManifest
+
+            const result = await window.bridge.ipcRenderer.invoke("make_install", packageManifest, version)
+
             if (!result) {
-                return reject(`Unknow error`)
+                return reject(`Unknown error`)
+            }
+
+            message.success(`[${packageId}] Package installed successfully`)
+
+            return resolve(true)
+        })
+    }
+
+    checkInstallation = async (id) => {
+        return await window.bridge.ipcRenderer.invoke("check_installation", id)
+    }
+
+    uninstallPackage = (id) => {
+        return new Promise(async (resolve, reject) => {
+            const result = await window.bridge.ipcRenderer.invoke("delete_installation", id)
+
+            if (!result) {
+                return reject(`Unknown error`)
+            }
+
+            return resolve(true)
+        })
+    }
+
+    openInstallationDir = (id) => {
+        return new Promise(async (resolve, reject) => {
+            const result = await window.bridge.ipcRenderer.invoke("open_installation_dir", id)
+
+            if (!result) {
+                return reject(`Unknown error`)
             }
 
             return resolve(true)
@@ -15,6 +47,9 @@ export default class SettingsExtension extends Extension {
     }
 
     window = {
-        "makeInstallation": this.makeInstallation 
+        "makeInstallation": this.makeInstallation,
+        "checkInstallation": this.checkInstallation,
+        "uninstallPackage": this.uninstallPackage,
+        "openInstallationDir": this.openInstallationDir,
     }
 }
