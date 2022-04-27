d9ad0b3012f817eda176a23ad6b73ca58653c4dc
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-27 21:12:37 +0200

    added `packages` lib

--- packages/gui/src/main/lib/packages.js
+++ packages/gui/src/main/lib/packages.js
@@ -2,7 +2,22 @@ import axios from "axios"
 
 function getPackageManifest(id) {
     return new Promise(async (resolve, reject) => {
-        const URI = `${window.bridge.originURI}/package/${id}`
+        const URI = `${global.SettingsDB.get("RepoServer")}/package/${id}`
+
+        const result = await axios.get(URI).catch(err => {
+            reject(err)
+            return false
+        })
+
+        if (result) {
+            return resolve(result.data)
+        }
+    })
+}
+
+function getEntrypointScript(id, version) {
+    return new Promise(async (resolve, reject) => {
+        const URI = `${global.SettingsDB.get("RepoServer")}/package/${id}@${version}/entrypoint`
 
         const result = await axios.get(URI).catch(err => {
             reject(err)
@@ -16,5 +31,6 @@ function getPackageManifest(id) {
 }
 
 export {
-    getPackageManifest
+    getPackageManifest,
+    getEntrypointScript,
 }
