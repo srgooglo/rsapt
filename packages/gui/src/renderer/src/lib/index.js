59bb9731679e4a6b7e8911c5ace88c13a59dd198
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-27 21:14:06 +0200

    clean lib

--- packages/gui/src/renderer/src/lib/index.js
+++ packages/gui/src/renderer/src/lib/index.js
@@ -8,13 +8,9 @@ export function sleep(duration) {
     return new Promise((resolve) => setTimeout(resolve, duration))
 }
 
-export async function validateVersionInstallation(version) {
-    return await window.bridge.ipcRenderer.invoke("check_version", version)
-}
-
 export function getPackages() {
     return new Promise(async (resolve, reject) => {
-        const URI = `${window.bridge.originURI}/packages`
+        const URI = `${window.app.config.RepoServer}/packages`
 
         const result = await axios.get(URI).catch(err => {
             reject(err)
@@ -29,7 +25,7 @@ export function getPackages() {
 
 export function getPackageData(packageId) {
     return new Promise(async (resolve, reject) => {
-        const URI = `${window.bridge.originURI}/package/${packageId}`
+        const URI = `${window.app.config.RepoServer}/package/${packageId}`
 
         const result = await axios.get(URI).catch(err => {
             reject(err)
@@ -39,72 +35,5 @@ export function getPackageData(packageId) {
         if (result) {
             return resolve(result.data)
         }
-    })
-}
-
-export function getPackageVersionManifest(packageId, version = "latest") {
-    return new Promise(async (resolve, reject) => {
-        const URI = `${window.bridge.originURI}/manifest/${packageId}@${version}`
-
-        const result = await axios.get(URI).catch(err => {
-            reject(err)
-            return false
-        })
-
-        if (result) {
-            return resolve(result.data)
-        }
-    })
-}
-
-export function installVersion(version) {
-    return new Promise(async (resolve, reject) => {
-        const URI = `${window.bridge.releasesURI}/${version}/manifest.json`
-
-        const manifest = await axios.get(URI).catch((error) => {
-            reject(`[404] Cannot get version data`)
-            return false
-        })
-
-        if (manifest) {
-            // first, download the version data
-            const result = await window.bridge.ipcRenderer.invoke("install_version", manifest.data).catch((error) => {
-                return false
-            })
-
-            if (!result) {
-                return reject()
-            }
-
-            return resolve(true)
-        }
-    })
-}
-
-export function playVersion(version) {
-    return new Promise(async (resolve, reject) => {
-        const result = await window.bridge.ipcRenderer.invoke("play_version", version).catch((error) => {
-            return false
-        })
-
-        if (!result) {
-            return reject()
-        }
-
-        return resolve(true)
-    })
-}
-
-export function deleteVersion(version) {
-    return new Promise(async (resolve, reject) => {
-        const result = await window.bridge.ipcRenderer.invoke("delete_version", version).catch((error) => {
-            return false
-        })
-
-        if (!result) {
-            return reject()
-        }
-
-        return resolve(true)
     })
 }
