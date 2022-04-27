848a703710fa050ab152c4b83c0e5b3ef97db47a
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-27 21:13:08 +0200

    added `setupDependencies` lib

--- packages/gui/src/main/lib/setupDependencies.js
+++ packages/gui/src/main/lib/setupDependencies.js
@@ -1,19 +1,32 @@
 import fs from "fs"
+import path from "path"
 import { _legacy_downloadHTTPSStream } from "./downloader"
 
 export default async () => {
     return new Promise(async (resolve, reject) => {
-        if (!fs.existsSync(global.BIN7z)) {
-            await fs.mkdirSync(global.BIN7z, { recursive: true })
+        const bin7zDirname = path.dirname(global.BIN7z)
 
-            await _legacy_downloadHTTPSStream(`https://dl.ragestudio.net/7z-bin/${global._7zScopedDist}/${global._7zBinExecutable}`, `${global.BIN7z}/${_7zBinExecutable}`).catch(err => {
-                console.error(err)
-                return reject(err)
-            })
-            await _legacy_downloadHTTPSStream("https://dl.ragestudio.net/7z-bin/7z.dll", `${global.BIN7z}/7z.dll`).catch(err => {
-                console.error(err)
-                return reject(err)
-            })
+        await fs.mkdirSync(bin7zDirname, { recursive: true })
+
+        await _legacy_downloadHTTPSStream(
+            `https://dl.ragestudio.net/7z-bin/${global._7zScopedDist}/${global._7zBinExecutable}`,
+            global.BIN7z,
+        ).catch(err => {
+            console.error(err)
+            return reject(err)
+        })
+        await _legacy_downloadHTTPSStream(
+            "https://dl.ragestudio.net/7z-bin/7z.dll",
+            `${bin7zDirname}/7z.dll`
+        ).catch(err => {
+            console.error(err)
+            return reject(err)
+        })
+
+        // fix permissions on unix systems
+        if (process.platform !== "win32") {
+            fs.chmodSync(global.BIN7z, "755")
+            fs.chmodSync(`${bin7zDirname}/7z.dll`, "755")
         }
 
         return resolve(true)
