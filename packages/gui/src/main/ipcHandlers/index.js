21443042a682719bceeda7726256f33d69416faa
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-27 21:09:49 +0200

    added index

--- packages/gui/src/main/ipcHandlers/index.js
+++ packages/gui/src/main/ipcHandlers/index.js
@@ -1,7 +1,14 @@
 import make_install from "./make_install"
+import check_installation from "./check_installation"
 import delete_installation from "./delete_installation"
+import open_installation_dir from "./open_installation_dir"
+
+import get_local_config from "./get_local_config"
 
 export default {
     make_install,
-    delete_installation
+    check_installation,
+    delete_installation,
+    open_installation_dir,
+    get_local_config,
 }
