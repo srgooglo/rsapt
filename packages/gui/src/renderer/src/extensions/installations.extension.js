dc3c42206379611a363cd11aca379f2e3064f830
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-28 21:39:48 +0200

    use `IPCInvoke`

--- packages/gui/src/renderer/src/extensions/installations.extension.js
+++ packages/gui/src/renderer/src/extensions/installations.extension.js
@@ -2,29 +2,56 @@ import { Extension } from "evite"
 import { message } from "antd"
 
 export default class InstallationsExtension extends Extension {
-    makeInstallation = (packageManifest, version) => {
+    makeInstallation = (packageManifest, version, params = {}) => {
+        const packageId = (typeof packageManifest === "object") ? packageManifest.name : packageManifest
+
+        // const onProgress = (progress) => {
+        //     if (typeof params.onProgress === "function") {
+        //         params.onProgress(progress)
+        //     }
+
+        //     window.app.eventBus.emit(`installation.progress.${packageId}`, progress)
+        // }
+
         return new Promise(async (resolve, reject) => {
-            const packageId = (typeof packageManifest === "object") ? packageManifest.name : packageManifest
+            window.IPCInvoke(
+                "make_install",
+                packageManifest,
+                version,
+            )
+                .catch((err) => {
+                    console.error(err)
+                    message.error(err)
 
-            const result = await window.bridge.ipcRenderer.invoke("make_install", packageManifest, version)
+                    window.app.eventBus.emit(`installation.error.${packageId}`, err)
 
-            if (!result) {
-                return reject(`Unknown error`)
-            }
+                    return reject(err)
+                })
+                .then((installation) => {
+                    console.log(installation)
+                    message.success(`[${installation.package.id}] Package installed successfully`)
 
-            message.success(`[${packageId}] Package installed successfully`)
+                    window.app.eventBus.emit(`installation.finished.${installation.package.id}`, installation)
 
-            return resolve(true)
+                    return resolve(installation)
+                })
+
+            message.info(`[New task] Installing ${packageId}@${version}`)
+
+            window.app.eventBus.emit(`installation.started`, {
+                packageId,
+            })
+            window.app.eventBus.emit(`installation.started.${packageId}`)
         })
     }
 
     checkInstallation = async (id) => {
-        return await window.bridge.ipcRenderer.invoke("check_installation", id)
+        return await window.IPCInvoke("check_installation", id)
     }
 
     uninstallPackage = (id) => {
         return new Promise(async (resolve, reject) => {
-            const result = await window.bridge.ipcRenderer.invoke("delete_installation", id)
+            const result = await window.IPCInvoke("delete_installation", id)
 
             if (!result) {
                 return reject(`Unknown error`)
@@ -36,7 +63,7 @@ export default class InstallationsExtension extends Extension {
 
     openInstallationDir = (id) => {
         return new Promise(async (resolve, reject) => {
-            const result = await window.bridge.ipcRenderer.invoke("open_installation_dir", id)
+            const result = await window.IPCInvoke("open_installation_dir", id)
 
             if (!result) {
                 return reject(`Unknown error`)
