2a3b75ba34b1d1c40f6c977ac784df87e5a77d04
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-04-28 21:40:46 +0200

    update index

--- packages/server/src/controllers/index.js
+++ packages/server/src/controllers/index.js
@@ -1,9 +1,7 @@
-import ManifestController from "./ManifestController"
 import PackageController from "./PackageController"
 import BundleController from "./BundleController"
 
 export default [
-    ManifestController,
     PackageController,
     BundleController,
 ]
