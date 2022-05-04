c90d8ec9dd2b94fc9ec5409c010a91937e60af5d
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-05-04 08:12:58 +0200

    move src to fixed dirname

--- packages/gui/configs/vite.renderer.ts
+++ packages/gui/configs/vite.renderer.ts
@@ -6,47 +6,36 @@ import path from "path"
 import { defineConfig } from "vite"
 import getConfig from "./.config.js"
 
-const config = getConfig()
+const fixed_src = path.resolve(__dirname, "../src/renderer/")
 
-const fixed__dirname = path.resolve(__dirname, "../src/renderer/")
+let config = getConfig()
 
-config.root = path.join(__dirname, "../src/renderer")
-
+config.root = fixed_src
 config.plugins = [
   react(),
   viteCommonjs()
 ]
-
-config.base = "./"
-
 config.build = {
   emptyOutDir: true,
   outDir: "../../dist/renderer",
   minify: process.env.NODE_ENV === "production",
-  // rollupOptions: {
-  //   external: [
-  //     ...builtinModules,
-  //     "electron",
-  //   ],
-  //   output: {
-  //     entryFileNames: "[name].cjs",
-  //   },
-  // },
+  rollupOptions: {
+    external: [
+      ...builtinModules,
+      "electron",
+    ],
+  },
 }
-
 config.resolve.alias = {
-  "~/": `${path.resolve(fixed__dirname, "src")}/`,
-  "__": fixed__dirname,
-  "@src": path.resolve(fixed__dirname, "src"),
-  lib: path.join(fixed__dirname, "src/lib"),
-  schemas: path.join(fixed__dirname, "schemas"),
-  extensions: path.resolve(fixed__dirname, "src/extensions"),
-  pages: path.join(fixed__dirname, "src/pages"),
-  theme: path.join(fixed__dirname, "src/theme"),
-  components: path.join(fixed__dirname, "src/components"),
-  models: path.join(fixed__dirname, "src/models"),
-  utils: path.join(fixed__dirname, "src/utils"),
+  "@src": fixed_src,
+  schemas: path.join(fixed_src, "schemas"),
+  lib: path.join(fixed_src, "lib"),
+  extensions: path.join(fixed_src, "extensions"),
+  pages: path.join(fixed_src, "pages"),
+  theme: path.join(fixed_src, "theme"),
+  components: path.join(fixed_src, "components"),
+  models: path.join(fixed_src, "models"),
+  utils: path.join(fixed_src, "utils"),
 }
 
-// https://vitejs.dev/config/
 export default defineConfig(config)
