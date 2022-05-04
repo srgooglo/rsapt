ce66674b58880c3ba2cd432d839b31bc4a18971e
srgooglo <38926803+srgooglo@users.noreply.github.com>
2022-05-04 08:19:20 +0200

    fix configs

--- packages/gui/configs/vite.renderer.ts
+++ packages/gui/configs/vite.renderer.ts
@@ -1,41 +1,54 @@
-import react from "@vitejs/plugin-react"
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs"
+import path from "path"
 import { builtinModules } from "module"
 
-import path from "path"
 import { defineConfig } from "vite"
-import getConfig from "./.config.js"
+import react from "@vitejs/plugin-react"
+import Pages from "vite-plugin-pages"
 
-const fixed_src = path.resolve(__dirname, "../src/renderer/")
+const fixed_src = path.resolve(__dirname, "../src/renderer")
 
-let config = getConfig()
-
-config.root = fixed_src
-config.plugins = [
-  react(),
-  viteCommonjs()
-]
-config.build = {
-  emptyOutDir: true,
-  outDir: "../../dist/renderer",
-  minify: process.env.NODE_ENV === "production",
-  rollupOptions: {
-    external: [
-      ...builtinModules,
-      "electron",
-    ],
+let config = {
+  mode: process.env.NODE_ENV,
+  root: fixed_src,
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      }
+    }
   },
-}
-config.resolve.alias = {
-  "@src": fixed_src,
-  schemas: path.join(fixed_src, "schemas"),
-  lib: path.join(fixed_src, "lib"),
-  extensions: path.join(fixed_src, "extensions"),
-  pages: path.join(fixed_src, "pages"),
-  theme: path.join(fixed_src, "theme"),
-  components: path.join(fixed_src, "components"),
-  models: path.join(fixed_src, "models"),
-  utils: path.join(fixed_src, "utils"),
+  plugins: [
+    react(),
+    Pages({
+      react: true,
+      routeStyle: "next",
+      extensions: ["jsx", "tsx"],
+    }),
+  ],
+  build: {
+    emptyOutDir: true,
+    outDir: "../../dist/renderer",
+    minify: process.env.NODE_ENV === "production",
+    rollupOptions: {
+      external: [
+        ...builtinModules,
+        "electron",
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@src": fixed_src,
+      schemas: path.join(fixed_src, "schemas"),
+      lib: path.join(fixed_src, "lib"),
+      extensions: path.join(fixed_src, "extensions"),
+      pages: path.join(fixed_src, "pages"),
+      theme: path.join(fixed_src, "theme"),
+      components: path.join(fixed_src, "components"),
+      models: path.join(fixed_src, "models"),
+      utils: path.join(fixed_src, "utils"),
+    }
+  }
 }
 
 export default defineConfig(config)
